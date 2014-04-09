b4b62f063197cef53681a229df888583e046b0ea
Eligijus Vitkauskas <eligijus.vitkauskas@oxid-esales.com>
2014-04-09 17:53:06 +0300

    Add linked_files and linked_dirs default options

--- lib/capistrano/oxid/defaults.rb
+++ lib/capistrano/oxid/defaults.rb
@@ -1,3 +1,22 @@
 # App path where OXID application is stored
 # Must have trailing "/" if specified
 set :app_path, ''
+
+# Files which are environment independent
+# On release path those files will be symlinked to shared folder
+set :linked_files, [
+  fetch(:app_path) + '.htaccess',
+  fetch(:app_path) + 'config.inc.php'
+]
+
+# Directories which are environment independent
+# On release path those directories will be symlinked to shared folder
+set :linked_dirs, [
+  fetch(:app_path) + 'log',
+  fetch(:app_path) + 'tmp',
+  fetch(:app_path) + 'cache',
+  fetch(:app_path) + 'export',
+  fetch(:app_path) + 'out/downloads',
+  fetch(:app_path) + 'out/media',
+  fetch(:app_path) + 'out/pictures'
+]
